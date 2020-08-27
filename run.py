70276768637057e11ba02ccef3a12a59107abfe0
Bernardo Costa <bernardoantunescosta@gmail.com>
2020-08-27 00:59:50 -0300

    Update run.py

--- run.py
+++ run.py
@@ -5,18 +5,18 @@ def main():
 
     '''Mandatory Parameters'''
     n_split = 10
-    n_components = 2
+    n_components = 6
     test_size = 0.2
     start_date = '2013/08/19'
-    data_path = './data/final-mix/data-1.csv'
-    columns = ['btc', 'gt', 'y']
-    epochs = 1
+    data_path = './data/final-mix/data-3.csv'
+    columns = ['btc1','btc2', 'btc3', 'gt1', 'gt2', 'gt3', 'y']
+    epochs = 100
     
     '''Algorithms Parameters'''
     algo_conf = {
         'name': 'lightgbm',
         'metric': 'accuracy'
-    }
+    } 
     
     
     r = R(algo_conf=algo_conf,
